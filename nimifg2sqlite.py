7f4870722e38f31aebf8e3ea1e664782dcab6065
Eric Zhangg <snow31450588@gmail.com>
2018-08-13 16:59:39 +0800

    import all mif files

--- nimifg2sqlite.py
+++ nimifg2sqlite.py
@@ -3,22 +3,23 @@
 
 import os
 import sys
+import glob
+
 import gdal
 import ogr
 
 
-def read_mif(file_name, datasource, province_name):
-    print(file_name)
+def read_mif(data_source, table_name, file_name):
+    print(table_name, file_name)
     ds = gdal.OpenEx(file_name, gdal.OF_VECTOR )
     if ds is None:
         print("Open failed.\n")
         sys.exit( 1 )
     layer_name = os.path.splitext(os.path.basename(file_name))[0]
-    table_name = layer_name[:-len(province_name)]
     layer_read = ds.GetLayerByName(layer_name)
     feat_defn = layer_read.GetLayerDefn()
 
-    layer_write = datasource.CreateLayer(table_name, None, layer_read.GetGeomType(), options = ['SPATIAL_INDEX=NO'] )
+    layer_write = data_source.CreateLayer(table_name, None, layer_read.GetGeomType(), options = ['SPATIAL_INDEX=NO'] )
     if layer_write is None:
         print("Layer creation failed.\n")
 
@@ -50,21 +51,25 @@ def create_datasource(driverName, file_name):
         return None
     return ds
 
+
 def folder_to_sqlite(fd, sqn):
-    driverName = "SQLite"
+    driver_name = "SQLite"
 
     province_name = os.path.basename(fd)
-    print(fd, sqn, province_name)
 
-    datasource = create_datasource(driverName, sqn)
+    data_source = create_datasource(driver_name, sqn)
 
-    if not datasource:
+    if not data_source:
         raise Exception("Create output failed!")
 
-    for table, sub in ((('R', 'road'), ('BL', 'back'), ('BN', 'back'), ('BP', 'back'), ('BUP', 'back'), ('D', 'back'))):
-        table_r = os.path.join(fd, sub, '%s%s.mif'%(table, province_name))
-        read_mif(table_r, datasource, province_name)
-
+    for sub in os.listdir(fd):
+        for mif in glob.glob(os.path.join(fd, sub, '*.mif')):
+            basename = os.path.basename(mif)
+            basename = os.path.splitext(basename)[0]
+            if len(basename)>len(province_name):
+                basename = basename[:-len(province_name)]
+            table_name = "%s_%s"%(sub, basename)
+            read_mif(data_source, table_name, mif)
 
 
 def usage():
